152738ac679a63c1bdb6343e06138df3ba5a73e8
Nicolas Penagos <nicolas.penagosm98@gmail.com>
2023-12-09 22:04:36 -0500

    Directives added

--- basic-components/src/app/server/server.component.ts
+++ basic-components/src/app/server/server.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {
-  serverId=10;
-  serverStatus="offline";
-  getServerStatus(){
+  serverId = 10;
+  serverStatus = 'offline';
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+  getServerStatus() {
     return this.serverStatus;
+  }
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
--- basic-components/src/app/servers/servers.component.ts
+++ basic-components/src/app/servers/servers.component.ts
@@ -13,7 +13,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName="No name"
+  serverName="No name";
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   }
   ngOnInit(): void {}
   onCreateServer(){
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is '+this.serverName;
   }
   onUpdateServerName(event:Event){
