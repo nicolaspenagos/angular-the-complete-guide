a264c111e1ca00c77cc3d763230b1ce608f8e3d5
Nicolas Penagos <nicolas.penagosm98@gmail.com>
2023-12-10 22:32:57 -0500

    Directives added

--- basic-components/src/app/servers/servers.component.ts
+++ basic-components/src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName="No name";
   serverCreated = false;
+  servers = ['Testserver1', 'Testserver2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
   onCreateServer(){
     this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is '+this.serverName;
+    this.servers.push(this.serverName);
   }
   onUpdateServerName(event:Event){
     this.serverName = (<HTMLInputElement>event.target).value;
